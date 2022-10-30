769f3626954ffd64e71d4ccb28f1c1c304cc3d2c
Hellsingi <il.dzemchanka@gmail.com>
2022-10-30 23:45:09 +0100

    feat: add serverless yml

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
-
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
 let server: Handler;
 
-// const port = process.env.PORT || 4000;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.init();
 
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
   app.use(helmet());
 
-  // await app.listen(port);
-  app.init();
-
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
 }
-
-// bootstrap().then(() => {
-//   console.log('App is running on %s port', port);
-// });
 
 export const handler: Handler = async (
   event: any,
