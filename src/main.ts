4d792058815751b4429a3f7c6b21831eb0576f44
Hellsingi <il.dzemchanka@gmail.com>
2022-11-06 22:03:22 +0100

    feat: add dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 
-import serverlessExpress from '@vendia/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
+import { AppModule } from './app.module';
 import helmet from 'helmet';
 
-import { AppModule } from './app.module';
-
-let server: Handler;
+const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.init();
-
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
   app.use(helmet());
-
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+  await app.listen(port);
 }
-
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
-};
+bootstrap().then(() => {
+  console.log('App is running on %s port', port);
+});
